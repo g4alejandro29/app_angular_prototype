60efbf35f07bce794b2417576348a632c59f274b
g4alejandro29 <g4alejandro29@gmail.com>
2019-10-12 21:43:41 -0600

    refactoring login

--- src/app/component/forgot/forgot.component.ts
+++ src/app/component/forgot/forgot.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-forgot',
@@ -6,10 +7,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot.component.scss']
 })
 export class ForgotComponent implements OnInit {
+  private user: string = null;
+  private submitted: boolean
+  private userError: string = null;
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    this.submitted = true
+    if(!this.validForm()) {
+      return false
+    }
+    this.router.navigate(['/login'])
+  }
+
+  validForm() {
+    this.user = this.user || ''
+    return (this.userIsValid())
+  }
+
+  userIsSet() {
+    return (this.user !== null)
+  }
+
+  userIsValid() {
+    if (!this.user && this.userIsSet()) {
+      this.userError = 'Usuario requerido.'
+      return false
+    }
+    this.userError = null
+    return true
+  }
+
+  userIsSuccess() {
+    return (this.userIsSet() && this.userIsValid())
+  }
+
+  userIsDanger() {
+    return (this.userIsSet() && !this.userIsValid())
+  }
+
+  updateUser(value: string) {
+    this.user = value
+  }
 }
